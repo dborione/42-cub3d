4cbb6b8e68d535404682311a3e0ff703afb7a66f
dborione <dborione@student.s19.be>
2024-01-19 15:59:19 +0100

    planification struct player

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -17,6 +17,14 @@ typedef struct	s_game {
 	void	*mlx_win;
 }				t_game;
 
+// typedef struct s_player {
+	// start pos(x, y)
+	// direction (x, y)
+	// field of view : plane perpendiculaire au player (x = 0, y)
+// }
+
+// Init
 int	ft_quit_window(t_game *g);
+int	ft_init_game(t_game *g, t_data *img);
 
 #endif
--- sources/start/main.c
+++ sources/start/main.c
@@ -5,15 +5,13 @@ int	main(void)
 	t_game	g;
 	t_data	img;
 
+
 	// init struct
 	// parse map
 		// init raycasting
+	// load textures
 	// movemnt
-	g.mlx = mlx_init();
-	g.mlx_win = mlx_new_window(g.mlx, 1920, 1080, "Cub3d");
-	img.img = mlx_new_image(g.mlx, 1920, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
+	ft_init_game(&g, &img);
 	//my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
 	mlx_put_image_to_window(g.mlx, g.mlx_win, img.img, 0, 0);
 	mlx_hook(g.mlx_win, 17, 0, ft_quit_window, &g);
