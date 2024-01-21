bb5ae79bc44e546fa5e9d36311faf32dc3cf54db
LeWeeky <leweeky@clappycrew.com>
2024-01-21 11:01:43 +0100

    ajout des textures (pas encore pris en charge)

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -1,8 +1,21 @@
-#ifndef CUB3D_H
-    # define CUB3D_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbarbiot <rbarbiot@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/21 10:57:07 by rbarbiot          #+#    #+#             */
+/*   Updated: 2024/01/21 11:00:22 by rbarbiot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include <mlx.h>
-#include <stdlib.h>
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <mlx.h>
+# include <stdlib.h>
+# include "cub3d_textures.h"
 
 typedef struct	s_data {
 	void	*img;
@@ -12,10 +25,22 @@ typedef struct	s_data {
 	int		endian;
 }				t_data;
 
+/* 
+	Cette structure est utilisée tout le long du jeu 
+	elle enregistre les éléments important tel que 
+	les pointeurs pour la MLX, les textures et code 
+	de la map
+*/
 typedef struct	s_game {
-	void	*mlx;
-	void	*mlx_win;
-}				t_game;
+	void				*mlx;
+	void				*mlx_win;
+	t_cub3d_textures	*textures;
+}						t_game;
+
+/*
+	Pour bien enregistrer la position du joueur il faudrait : 
+	x + y (coordonées en float) et pitch + yaw (direction en float)
+*/
 
 // typedef struct s_player {
 	// start pos(x, y)
---
+++ includes/cub3d_textures.h
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d_textures.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rbarbiot <rbarbiot@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/21 10:26:19 by rbarbiot          #+#    #+#             */
+/*   Updated: 2024/01/21 10:56:14 by rbarbiot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_TEXTURES_H
+# define CUB3D_TEXTURES_H
+
+/* Défini des codes pour les zones sur la map */
+
+# define CUB3D_OUTSIDE 32
+# define CUB3D_EMPTY 48
+# define CUB3D_WALL 49
+# define CUB3D_SPAWN_NORTH 78
+# define CUB3D_SPAWN_SOUTH 83
+# define CUB3D_SPAWN_WEST 87
+# define CUB3D_SPAWN_EAST 69
+
+/* Type RGB pour définir les couleurs */
+
+typedef struct s_rgb
+{
+	int	red;
+	int green;
+	int blue;
+}				t_rgb;
+
+/* Enregistre toutes les textures nécessaire */
+
+typedef struct	s_cub3d_textures
+{
+	void	*north_texture;
+	void	*south_texture;
+	void	*west_texture;
+	void	*east_texture;
+	t_rgb	*floor;
+	t_rgb	*ceiling;
+	char		**map[];
+}				t_cub3d_textures;
+
+#endif
