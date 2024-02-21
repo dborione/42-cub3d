563b5d05f8dd0406a795282928e54d6374faacc7
rbarbiot <rbarbiot@student.s19.be>
2024-02-21 15:12:15 +0100

    ajout de protection contre les maps invalides (pas de murs ouest / est) ajout du nouveau systeme de coordonnees (plus precis / efficace) du joueur

--- includes/cub3d_loading.h
+++ includes/cub3d_loading.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d_loading.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rbarbiot <rbarbiot@student.s19.be>         +#+  +:+       +#+        */
+/*   By: rbarbiot <rbarbiot@student.19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 10:05:10 by rbarbiot          #+#    #+#             */
-/*   Updated: 2024/02/05 14:27:23 by rbarbiot         ###   ########.fr       */
+/*   Updated: 2024/02/21 14:17:05 by rbarbiot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,10 @@ int		ft_load_game(t_game *game, char *map_path);
 int 	ft_load_element(t_game *game, char *line);
 int		ft_load_color(t_game *game, char *line, char target);
 void	ft_load_map_schema(t_game *game, int fd);
-int		ft_load_player_location(t_game *game);
+int		ft_parse_map(t_game *game);
 void	ft_unload_game(t_game *game);
 int		ft_get_int_rgb(t_rgb *rgb);
+int		ft_has_west_wall(char *line);
+int		ft_has_east_wall(char *line);
 
 #endif
