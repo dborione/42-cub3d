37c152a3f492db8e0a3eb89a10b0b070a6e3f8a9
LeWeeky <leweeky@clappycrew.com>
2024-03-13 11:58:15 +0100

    amélioration du draw au niveau des normes avec la fonction get_target

--- sources/render/draw.c
+++ sources/render/draw.c
@@ -6,62 +6,35 @@
 /*   By: rbarbiot <rbarbiot@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 11:39:46 by rbarbiot          #+#    #+#             */
-/*   Updated: 2024/03/08 01:46:18 by rbarbiot         ###   ########.fr       */
+/*   Updated: 2024/03/13 11:57:21 by rbarbiot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 #include "../../includes/cub3d_raycasting.h"
 
-// void ft_draw_frame(t_game *game)
-// {
-// 	t_cub3d_images	frame;
-// 	t_location		pen_location;
-// 	int				i;
-
-// 	frame.data = mlx_get_data_addr(game->textures->test,
-// 		&frame.bits_per_pixel, &frame.size_line, &frame.endian);
-
-// 	pen_location.y = 0;
-// 	i = 0;
-// 	while (frame.data[i] && pen_location.y < WIN_HEIGHT)
-// 	{
-// 		pen_location.x = 0;
-// 		while (pen_location.x < frame.size_line)
-// 		{
-// 			int rgb_color = 0;
-// 			frame.data[i] = rgb_color;
-// 			pen_location.x++;
-// 			i++;
-// 		}
-// 		pen_location.y++;
-// 	}
-// 	mlx_put_image_to_window(game->mlx, game->mlx_win, game->textures->test, 0, 0);
-// }
+static
+void	*ft_get_target(t_game *game, t_raycaster *raycaster)
+{
+	if (raycaster->ray->side == NORTH_WALL)
+		return (game->textures->north_texture);
+	else if (raycaster->ray->side == WEST_WALL)
+		return (game->textures->west_texture);
+	else if (raycaster->ray->side == EAST_WALL)
+		return (game->textures->east_texture);
+	return (game->textures->south_texture);
+}
 
 void ft_draw_vertical_line(t_game *game, t_raycaster *raycaster, int x) // ajouter le target ici
 {
 	t_cub3d_images	texture;
-	void			*target;
 	int				factor;
 	int				count;
 	int				line;
 	int				remainder;
 
-	if (raycaster->ray->side == NORTH_WALL)
-		target = game->textures->north_texture;
-	else if (raycaster->ray->side == WEST_WALL)
-		target = game->textures->west_texture;
-	else if (raycaster->ray->side == EAST_WALL)
-		target = game->textures->east_texture;
-	else
-		target = game->textures->south_texture;
-	texture.data = mlx_get_data_addr(target,
+	texture.data = mlx_get_data_addr(ft_get_target(game, raycaster),
 		&texture.bits_per_pixel, &texture.size_line, &texture.endian);
-	/*
-		colomn_size pourra etre retiré car on ne l'utilise qu'une seule fois
-		mais je la garde pour le moment pour avoir un
-	*/
 	factor = raycaster->line->height / WALL_HEIGHT;
 	remainder = raycaster->line->height % WALL_HEIGHT;
 	// ft_printf("factor : top %d - bottom : %d / wall %d = %d, rest : %d, Height : %d\n",
