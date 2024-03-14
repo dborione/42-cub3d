eef99e5858c9367d9e148be8c7d8f3a4c4d5d8bc
rbarbiot <rbarbiot@student.s19.be>
2024-03-14 17:12:08 +0100

    patch du bug des textures qui changes de taille

--- sources/render/draw.c
+++ sources/render/draw.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   draw.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rbarbiot <rbarbiot@student.s19.be>         +#+  +:+       +#+        */
+/*   By: rbarbiot <rbarbiot@student.19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 11:39:46 by rbarbiot          #+#    #+#             */
-/*   Updated: 2024/03/14 14:33:08 by rbarbiot         ###   ########.fr       */
+/*   Updated: 2024/03/14 17:10:43 by rbarbiot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,20 @@ void	*ft_get_target(t_game *game, t_raycaster *raycaster)
 	return (game->textures->south_texture);
 }
 
+static
+void    ft_get_wall_hit_point(t_game *game, t_raycaster *raycaster)
+{
+    raycaster->line->texture_map_pos = game->textures->map[(int)raycaster->player_pos_y][(int)raycaster->player_pos_x] - 1;
+    raycaster->line->wall_hit_x = 0;
+    if (raycaster->ray->side == EAST_WALL || raycaster->ray->side == WEST_WALL)
+        raycaster->line->wall_hit_x = raycaster->player_pos_y +
+            raycaster->ray->ray_to_wall_dist * raycaster->ray->dir_y;
+    else
+        raycaster->line->wall_hit_x = raycaster->player_pos_x +
+            raycaster->ray->ray_to_wall_dist * raycaster->ray->dir_x;
+    raycaster->line->wall_hit_x -= floor(raycaster->line->wall_hit_x); // double floor(double x) = returns the largest integer value less than or equal to x
+}
+
 void	ft_draw_vertical_line(t_game *game, t_raycaster *raycaster, int x)
 {
 	t_cub3d_images	texture;
@@ -38,21 +52,17 @@ void	ft_draw_vertical_line(t_game *game, t_raycaster *raycaster, int x)
 	{
 		int texY = (int)texture_position & (WALL_HEIGHT - 1);
         texture_position += step;
-		double wallX; //where exactly the wall was hit
-		if (raycaster->ray->side == NORTH_WALL && raycaster->ray->side == SOUTH_WALL)
-			wallX = raycaster->player_pos_y + raycaster->ray->ray_to_wall_dist * raycaster->ray->delta_dist_y;
-		else
-			wallX = raycaster->player_pos_x + raycaster->ray->ray_to_wall_dist * raycaster->ray->delta_dist_x;
-		wallX -= floor((wallX));
-		int texX = (int)(wallX * (double)WALL_WIDTH);
+		 //where exactly the wall was hit
+		ft_get_wall_hit_point(game, raycaster);
+		int texX = (int)(raycaster->line->wall_hit_x * (double)WALL_WIDTH);
 		if ((raycaster->ray->side == NORTH_WALL || raycaster->ray->side == SOUTH_WALL) && raycaster->ray->dir_x > 0)
 			texX = WALL_WIDTH - texX - 1;
 		if ((raycaster->ray->side == WEST_WALL || raycaster->ray->side == EAST_WALL) && raycaster->ray->dir_y < 0)
 			texX = WALL_WIDTH - texX - 1;
-		game->textures->frame->data[raycaster->line->bottom * game->textures->frame->size_line + x*4] = texture.data[texX * 4 + texture.size_line  * texY];
-		game->textures->frame->data[raycaster->line->bottom * game->textures->frame->size_line + x*4 + 1] = texture.data[texX * 4 + 1 + texture.size_line  * texY];
-		game->textures->frame->data[raycaster->line->bottom * game->textures->frame->size_line + x*4 + 2] = texture.data[texX * 4 + 2 + texture.size_line  * texY];
-		game->textures->frame->data[raycaster->line->bottom * game->textures->frame->size_line + x*4 + 3] = texture.data[texX * 4 + 3 + texture.size_line  * texY];
+		game->textures->frame->data[raycaster->line->bottom * game->textures->frame->size_line + x * 4] = texture.data[texX * 4 + texture.size_line * texY];
+		game->textures->frame->data[raycaster->line->bottom * game->textures->frame->size_line + x * 4 + 1] = texture.data[texX * 4 + 1 + texture.size_line * texY];
+		game->textures->frame->data[raycaster->line->bottom * game->textures->frame->size_line + x * 4 + 2] = texture.data[texX * 4 + 2 + texture.size_line * texY];
+		game->textures->frame->data[raycaster->line->bottom * game->textures->frame->size_line + x * 4 + 3] = texture.data[texX * 4 + 3 + texture.size_line * texY];
 		raycaster->line->bottom++;
  	}
 }
