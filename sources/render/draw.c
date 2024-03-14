f7e625268fe45ae674724e6f04ca93fbd6bec28b
LeWeeky <leweeky@clappycrew.com>
2024-03-14 13:31:16 +0100

    bug d'affichage suite au passage du draw en raycasting

--- sources/render/draw.c
+++ sources/render/draw.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   draw.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rbarbiot <rbarbiot@student.19.be>          +#+  +:+       +#+        */
+/*   By: rbarbiot <rbarbiot@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 11:39:46 by rbarbiot          #+#    #+#             */
-/*   Updated: 2024/03/13 15:55:12 by rbarbiot         ###   ########.fr       */
+/*   Updated: 2024/03/14 13:26:29 by rbarbiot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,56 +29,40 @@ void	*ft_get_target(t_game *game, t_raycaster *raycaster)
 void	ft_draw_vertical_line(t_game *game, t_raycaster *raycaster, int x, t_wall_measures *wall_measures)
 {
 	t_cub3d_images	texture;
-	int				count;
-	int				line;
+	// int				line;
 
+	if (wall_measures)
+		{
 
-	if (x - wall_measures->start == wall_measures->width_factor * (wall_measures->column + 1)
-	&& wall_measures->column + 1 < WALL_WIDTH)
-		wall_measures->column++;
-
+		}
+	
+	double			step = 1.0 * WALL_HEIGHT / raycaster->line->height;
+	double			texture_position = (raycaster->line->bottom - WALL_HEIGHT / 2 + raycaster->line->height / 2) * step;
+	//printf("texture position %f, %f\n", step, texture_position);
 	texture.data = mlx_get_data_addr(ft_get_target(game, raycaster),
 		&texture.bits_per_pixel, &texture.size_line, &texture.endian);
-	wall_measures->height_factor = raycaster->line->height / WALL_HEIGHT;
-	wall_measures->height_remainder = raycaster->line->height % WALL_HEIGHT;
-	// ft_printf("factor : top %d - bottom : %d / wall %d = %d, rest : %d, Height : %d\n",
-	// 	raycaster->line->top, raycaster->line->bottom, WALL_HEIGHT, factor, remainder, raycaster->line->height);
-	line = 0;
-	count = 0;
-	while (count < raycaster->line->height - (raycaster->line->top - raycaster->line->bottom))
-		count++;
-	if (count)
-		count /= 2;
-	while (count && count - wall_measures->height_factor > 0)
+	//printf("top : %d, botton : %d\n", raycaster->line->top, raycaster->line->bottom);
+	while (raycaster->line->bottom < raycaster->line->top)
 	{
-		line++;
-		count -= wall_measures->height_factor;
-	}
-	while (raycaster->line->bottom < raycaster->line->top && line < WALL_HEIGHT)
-	{
-		//if (line && rest && line % rest)
-		if (line > WALL_HEIGHT - wall_measures->height_remainder || line < wall_measures->height_remainder)
-		{
-			//while (rest)
-			{
-				game->textures->frame->data[raycaster->line->bottom * game->textures->frame->size_line + x*4] = texture.data[wall_measures->column * 4 + texture.size_line  * line];
-				game->textures->frame->data[raycaster->line->bottom * game->textures->frame->size_line + x*4 + 1] = texture.data[wall_measures->column * 4 + 1 + texture.size_line  * line];
-				game->textures->frame->data[raycaster->line->bottom * game->textures->frame->size_line + x*4 + 2] = texture.data[wall_measures->column * 4 + 2 + texture.size_line  * line];
-				game->textures->frame->data[raycaster->line->bottom * game->textures->frame->size_line + x*4 + 3] = texture.data[wall_measures->column * 4 + 3 + texture.size_line  * line];
-				raycaster->line->bottom++;
-				wall_measures->height_remainder--;
-			}
-		}
-		while (count < wall_measures->height_factor && raycaster->line->bottom < raycaster->line->top)// faire attention aux facteurs negatifs
-		{
-			game->textures->frame->data[raycaster->line->bottom * game->textures->frame->size_line + x*4] = texture.data[wall_measures->column * 4 + 0 + texture.size_line  * line];
-			game->textures->frame->data[raycaster->line->bottom * game->textures->frame->size_line + x*4 + 1] = texture.data[wall_measures->column * 4 + 1 + texture.size_line  * line];
-			game->textures->frame->data[raycaster->line->bottom * game->textures->frame->size_line + x*4 + 2] = texture.data[wall_measures->column * 4 + 2 + texture.size_line  * line];
-			game->textures->frame->data[raycaster->line->bottom * game->textures->frame->size_line + x*4 + 3] = texture.data[wall_measures->column * 4 + 3 + texture.size_line  * line];
-			raycaster->line->bottom++;
-			count++;
-		}
-		line++;
-		count = 0;
+		int texY = (int)texture_position & (WALL_HEIGHT - 1);
+        texture_position += step;
+		double wallX; //where exactly the wall was hit
+		if (raycaster->ray->side == NORTH_WALL && raycaster->ray->side == SOUTH_WALL)
+			wallX = raycaster->player_pos_y + raycaster->ray->ray_to_wall_dist * raycaster->ray->delta_dist_y;
+		else
+			wallX = raycaster->player_pos_x + raycaster->ray->ray_to_wall_dist * raycaster->ray->delta_dist_x;
+		wallX -= floor((wallX));
+
+		//x coordinate on the texture
+		int texX = (int)(wallX * (double)WALL_WIDTH);
+		if ((raycaster->ray->side == NORTH_WALL && raycaster->ray->side == SOUTH_WALL) && raycaster->ray->dir_x > 0)
+			texX = WALL_WIDTH - texX - 1;
+		if ((raycaster->ray->side == WEST_WALL && raycaster->ray->side == EAST_WALL) && raycaster->ray->dir_y < 0)
+			texX = WALL_WIDTH - texX - 1;
+		game->textures->frame->data[raycaster->line->bottom * game->textures->frame->size_line + x*4] = texture.data[texX * 4 + texture.size_line  * texY];
+		game->textures->frame->data[raycaster->line->bottom * game->textures->frame->size_line + x*4 + 1] = texture.data[texX * 4 + 1 + texture.size_line  * texY];
+		game->textures->frame->data[raycaster->line->bottom * game->textures->frame->size_line + x*4 + 2] = texture.data[texX * 4 + 2 + texture.size_line  * texY];
+		game->textures->frame->data[raycaster->line->bottom * game->textures->frame->size_line + x*4 + 3] = texture.data[texX * 4 + 3 + texture.size_line  * texY];
+		raycaster->line->bottom++;
  	}
 }
